588fbbfd110fa9bc50f15ab875994b164f02b21c
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2024-11-15 00:20:06 -0500

    first commit

---
+++ src/test/java/com/appweb/crud/estudiante/crud_estudiante/CrudEstudianteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appweb.crud.estudiante.crud_estudiante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEstudianteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
