f0bfae5f4a24cdbc135ac31b79a2f69a533e3335
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2024-11-15 11:00:12 -0500

    Class Student finished

--- src/main/java/com/appweb/crud/estudiante/crud_estudiante/entitie/Student.java
+++ src/main/java/com/appweb/crud/estudiante/crud_estudiante/entitie/Student.java
@@ -16,10 +16,10 @@ public class Student {
     private Long id;
 
     @Column(name="name",nullable = false,length = 50)
-    private String nombre;
+    private String name;
 
-    @Column(name = "apellidos",nullable = false, length = 50)
-    private String apellidos;
+    @Column(name = "lastName",nullable = false, length = 50)
+    private String lastName;
 
     @Column(name = "email",nullable = false, length = 50, unique = true)
     private String email;
@@ -27,11 +27,48 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String nombre, String apellidos, String email) {
+    public Student(Long id, String name, String lastName, String email) {
         this.id = id;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
+        this.name = name;
+        this.lastName = lastName;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", lastName=" + lastName + ", email=" + email + "]";
     }
 
     
